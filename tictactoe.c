0f82c419bbf9c5a3b7d054b3c9a43fb7a1261f21
Simon <93432414+simonsclz@users.noreply.github.com>
2022-03-27 12:10:55 +0200

    Add files via upload

--- tictactoe.c
+++ tictactoe.c
@@ -7,35 +7,47 @@
 #include "ttt_bot_hard.h"
 
 int main(int argc, char *args[]) {
-    unsigned short currentState = NOT_DECIDED;
-    char decision;
+    unsigned short currentState = NOT_DECIDED, turn;
+    char decision, playerLetter, botLetter, enter;
     printf("Do you want to start? [y/n]\n");
-    scanf("%c", &decision);
-    unsigned short currentLetter;
-    if(decision == 'y') currentLetter = O;
-    if(decision == 'n') currentLetter = X;
+    scanf("%c%c", &decision, &enter);
+    printf("Which letter do you want to use? [X/O]\n");
+    scanf("%c%c", &playerLetter, &enter);
+    while(playerLetter != 'X' && playerLetter != 'O') {
+        printf("Please enter a valid value!\n");
+        scanf("%c%c", &playerLetter, &enter);
+    }
+    if(playerLetter == 'O') playerLetter = O, botLetter = X; // functions only work with enum-letters
+    if(playerLetter == 'X') playerLetter = X, botLetter = O;
+    if(decision == 'y') turn = PLAYER;
+    if(decision == 'n') turn = BOT;
     while(currentState == NOT_DECIDED) {
         printGrid(grid);
-        if(currentLetter == O) {
-            inputGrid(grid, currentLetter);
+        if(turn == PLAYER) {
+            inputGrid(grid, playerLetter);
         } else {
-            midBot(grid, currentLetter);
+            midBot(grid, botLetter);
         }
         printGrid(grid);
         currentState = evaluate(grid);
-        if(currentLetter == X) {
-            currentLetter = O;
+        if(turn == PLAYER) {
+            turn = BOT;
         } else {
-            currentLetter = X;
+            turn = PLAYER;
         }
         printf("\n\n");
     }
+    char currentLetter;
     if(currentState == DRAW) printf("Nobody has won. It's a draw.\n"), printGrid(grid);
-    if(currentLetter == X) {
-            currentLetter = O;
+    if(turn == PLAYER) {
+            turn = BOT;
+            if(botLetter == X) currentLetter = 'X';
+            if(botLetter == O) currentLetter = 'O';
         } else {
-            currentLetter = X;
+            turn = PLAYER;
+            if(playerLetter == X) currentLetter = 'X';
+            if(playerLetter == O) currentLetter = 'O';
         }
-    if(currentState == WON && currentLetter == X) printf("X has won!\n"), printGrid(grid);
-    if(currentState == WON && currentLetter == O) printf("O has won!\n"), printGrid(grid);
+    if(currentState == WON && turn == BOT) printf("%c has won!\n", currentLetter), printGrid(grid);
+    if(currentState == WON && turn == PLAYER) printf("%c has won!\n", currentLetter), printGrid(grid);
 }
--- ttt_types.h
+++ ttt_types.h
@@ -1,3 +1,4 @@
 char grid[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
 enum letters {O, X};
 enum game {DRAW, NOT_DECIDED, WON};
+enum state {PLAYER, BOT};
